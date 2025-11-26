f01e070e100c676a6a9e9e45c712869efc3f590d
praveena-pawar <praveena.dev02@gmail.com>
2025-11-26 12:04:02 +0530

    Add DAY-1 logic building exercises

---
+++ 01_variables.py
@@ -0,0 +1,13 @@
+# ✅ DAY-1: Logic Building Exercises
+
+# Q1. Predict the output
+x = 5
+y = 2
+
+x = x + y      # step 1
+y = x - y      # step 2
+x = x - y      # step 3
+
+print("Q1 output:", x, y)
+
+# and the out put is x = 2 and y = 5

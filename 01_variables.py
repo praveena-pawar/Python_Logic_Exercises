9d336d6b7636ad77b0ede358dd4cf7bbfd493576
praveena-pawar <praveena.dev02@gmail.com>
2025-11-27 12:14:22 +0530

    predict output

--- 01_variables.py
+++ 01_variables.py
@@ -25,3 +25,25 @@ a = a // b     # step 3
 print("Q2 output:", a, b)
 
 # and the out put is a = 3 and b = 10
+
+
+
+
+
+
+# Q3. Predict the Output
+x = 4
+y = 6
+z = 2
+
+x = x + y      # step 1
+y = y + z      # step 2
+z = x - y      # step 3
+x = z + y      # step 4
+
+
+print("Q3 output :", x, y, z)
+
+# and the output is x = 10, y = 8, z = 2
+
+

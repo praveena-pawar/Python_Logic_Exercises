1e5bbaa9540c5d997c2d4340e57a98de5edb673e
praveena-pawar <praveena.dev02@gmail.com>
2025-11-28 12:11:32 +0530

    predict the output

--- conditional_statements.py
+++ conditional_statements.py
@@ -10,3 +10,22 @@ else:
 print("Q1 output :", x, y)
 
 # and the output is x = 15 and y = 5
+
+
+
+
+
+# Q2. Slightly harder (with elif)
+a = 7
+b = 12
+
+if a > b:
+    a = a - b
+elif b > a:
+    b = b - a
+else:
+    a = a + b
+
+print("Q2 output :", a, b)
+
+# and the output is a = 7 and b = 5

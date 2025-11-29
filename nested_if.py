3ebcc91ad8b721649ca5e60aab0e911502fb25fc
praveena-pawar <praveena.dev02@gmail.com>
2025-11-29 13:11:15 +0530

    Tricky Conditional + Logical Operators + Nested If

--- nested_if.py
+++ nested_if.py
@@ -15,3 +15,27 @@ else:
 print("Q1 output :", a, b, c)
 
 # and the output a = 20, b = 5 and c = 12
+
+
+
+
+
+
+
+
+# Q2. Tricky Conditional + Logical Operators + Nested If
+x = 7
+y = 10
+z = 5
+
+if x + y > z * 3:
+    if y - x < z:
+        x = x + y
+    else:
+        y = y + z
+else:
+    z = z + x
+
+print("Q2 output :", x, y, z)
+
+# and the ouptut x = 17, y = 10, z = 5

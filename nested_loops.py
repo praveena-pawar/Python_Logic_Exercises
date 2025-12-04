9556fc8c09b7ee5bfc80a3f319586546011aa722
praveena-pawar <praveena.dev02@gmail.com>
2025-12-04 05:44:55 +0530

    Nested Loop Predict the Output

---
+++ nested_loops.py
@@ -0,0 +1,12 @@
+# Q1. Nested Loop Predict the Output    
+total = 0
+
+for i in range(1, 4):      # i = 1,2,3
+    for j in range(1, 3):  # j = 1,2
+        total += i * j
+
+print("Q1 output :", total)
+
+#and the output total = 18
+
+

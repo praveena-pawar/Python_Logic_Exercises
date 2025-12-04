7239fa78b092c23676dae1f4b64f45967a8f0a05
praveena-pawar <praveena.dev02@gmail.com>
2025-12-04 05:48:26 +0530

    Nested Loops with If Condition

--- nested_loops.py
+++ nested_loops.py
@@ -10,3 +10,18 @@ print("Q1 output :", total)
 #and the output total = 18
 
 
+
+
+# Q2. Nested Loops with If Condition
+total = 0
+
+for i in range(1, 4):       # i = 1,2,3
+    for j in range(1, 4):   # j = 1,2,3
+        if (i + j) % 2 == 0:
+            total += i * j
+        else:
+            total -= (i + j)
+
+print("Q2 output :", total)
+
+# and the output total 4

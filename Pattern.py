9fd739128bb46a359212c9e100eb6f46e4da7e1b
praveena-pawar <praveena.dev02@gmail.com>
2025-12-07 22:52:13 +0530

    output for Q2

--- Pattern.py
+++ Pattern.py
@@ -5,7 +5,9 @@ for i in range(1, 4):
     print()
 
 # Q1 output
-#  
+# *
+# **
+# ***
 
 
 
@@ -14,3 +16,9 @@ for i in range(1, 5):          # i = 1,2,3,4
     for j in range(1, i+1):     # j = 1 to i
         print(j, end="")
     print()
+
+# Q2 output 
+# 1
+# 12
+# 123
+# 1234

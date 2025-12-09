fbaa3f8b990a6cbb1ea604cc7433440204a6cc01
praveena-pawar <praveena.dev02@gmail.com>
2025-12-09 20:18:31 +0530

    Reverse Triangle

--- Pattern.py
+++ Pattern.py
@@ -36,3 +36,16 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end="")
     print()
+
+# Q3 output 
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+
+n = 5
+for i in range(n, 0, -1):
+    print("*" * i)
